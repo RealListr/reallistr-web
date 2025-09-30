a0f38d4e9ef99538c26cb97c77b64eb7469e1f40
RealListr <support@ziioz.com>
2025-09-30 06:50:26 +0000

    fix(vercel): dynamic import react-map-gl with ssr:false

--- src/components/map/MapPreview.tsx
+++ src/components/map/MapPreview.tsx
@@ -1,19 +1,26 @@
 'use client';
-import Map, { Marker, NavigationControl } from 'react-map-gl';
+
+import dynamic from 'next/dynamic';
 import 'mapbox-gl/dist/mapbox-gl.css';
 
+// Dynamically import to avoid SSR issues on Vercel
+const MapGL = dynamic(() => import('react-map-gl').then(m => m.default), { ssr: false });
+const Marker = dynamic(() => import('react-map-gl').then(m => m.Marker), { ssr: false });
+const NavigationControl = dynamic(() => import('react-map-gl').then(m => m.NavigationControl), { ssr: false });
+
 type MapPreviewProps = { lat: number; lng: number; height?: number | string };
+
 export default function MapPreview({ lat, lng, height = 260 }: MapPreviewProps) {
   return (
     <div className="rounded-2xl overflow-hidden border" style={{ height }}>
-      <Map
+      <MapGL
         mapboxAccessToken={process.env.NEXT_PUBLIC_MAPBOX_TOKEN}
         initialViewState={{ longitude: lng, latitude: lat, zoom: 14 }}
         mapStyle="mapbox://styles/mapbox/streets-v12"
       >
         <NavigationControl position="top-left" />
         <Marker longitude={lng} latitude={lat} />
-      </Map>
+      </MapGL>
     </div>
   );
 }
