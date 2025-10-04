462037256cf49b157e7c189c154de0da1a316de4
RealListr <support@ziioz.com>
2025-10-04 05:58:32 +0000

    stable-rail: fix stray paren in index.tsx + ensure default export in _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,6 @@
-import React from "react";
 import type { AppProps } from "next/app";
 import "../app/globals.css";
 
-import FloorPlanOverlay from "@/components/rail/FloorPlanOverlay";
-import FloorPlanCapture from "@/components/rail/FloorPlanCapture";
-
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <FloorPlanOverlay />
-      <FloorPlanCapture defaultSrc="/images/floorplan-123.png" />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
